ca9d40068581e696c18db0076052ee46fe5aa361
Henry Garcia <hgarcia8889@gmail.com>
2025-04-30 14:00:59 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Part 1: Fizz Buzz
+console.log("--Part 1: Fizz Buzz--")
+for (let index = 1; index <= 100; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+        console.log("Fizz Buzz")
+    }else if(index % 3 === 0){
+        console.log("Fizz")
+    }else if (index % 5 === 0) {
+        console.log("Buzz")
+    }else {
+        console.log(index)
+    }
+    
+}
