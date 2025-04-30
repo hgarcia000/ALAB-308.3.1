309a43dbf7d3d15bc27df9ec3520cb69531a1693
Henry Garcia <hgarcia8889@gmail.com>
2025-04-30 14:16:08 -0400

    third commit

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ for (let index = 1; index <= 100; index++) {
     }
     
 }
+
+// Part 2: Prime Time
+console.log("--Part 2: Prime Time--")
+let n = 120;
+let d = 0;
+while (d !== 2) {
+    d = 0;
+    n++;
+    for(let i = 1; i <= n; i++){
+        if (n % i === 0) {
+            d++
+        }
+    }
+    if(d === 2){
+        console.log(n);
+        break;
+    }
+}
